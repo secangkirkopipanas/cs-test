2ac6b6bfb77184cadf1bbec995130cb4273ac411
Robertus Lilik Haryanto <robert.djokdja@gmail.com>
2018-04-04 17:53:15 +0800

    Improved unit test

--- src/test/java/com.secangkirkopipanas.cstest.test/URLCheckerTest.java
+++ src/test/java/com.secangkirkopipanas.cstest.test/URLCheckerTest.java
@@ -18,6 +18,13 @@ public class URLCheckerTest {
     private String url2 = "http://www.yahoo.com";
 
     @Test
+    public void testGetStatusString() throws IOException {
+        Assert.assertEquals("UNAUTHORIZED", urlChecker.getStatusString(401, null));
+        Assert.assertEquals("FORBIDDEN", urlChecker.getStatusString(403, null));
+        Assert.assertEquals("NOT AVAILABLE", urlChecker.getStatusString(404, null));
+    }
+
+    @Test
     public void testGetResponse() throws IOException {
         CloseableHttpResponse httpResponse = urlChecker.getResponse(url1);
         Assert.assertNotNull(httpResponse);
@@ -29,6 +36,7 @@ public class URLCheckerTest {
     @Test
     public void testHealthStatus() throws IOException, InterruptedException {
         // URLChecker.healthStatus() test with single URL
+        Assert.assertTrue(urlChecker.healthStatus(url1).contains("GREEN"));
         Assert.assertTrue(urlChecker.healthStatus(url1, 1, 1).contains("GREEN"));
         Assert.assertTrue(urlChecker.healthStatus(url1, 1, 1, "Component Status: GREEN").contains("BLUE"));
         Assert.assertTrue(urlChecker.healthStatus(url1, 1, 1, "<style>").contains("GREEN"));
@@ -37,6 +45,8 @@ public class URLCheckerTest {
         List<String> urls = new ArrayList<>();
         urls.add(url1);
         urls.add(url2);
+        Assert.assertTrue(urlChecker.healthStatus(urls).contains("GREEN,http://www.yahoo.com"));
+        Assert.assertTrue(urlChecker.healthStatus(urls).contains("GREEN,http://www.google.com"));
         Assert.assertTrue(urlChecker.healthStatus(urls, 1, 1).contains("GREEN,http://www.yahoo.com"));
         Assert.assertTrue(urlChecker.healthStatus(urls, 1, 1).contains("GREEN,http://www.google.com"));
         Assert.assertTrue(urlChecker.healthStatus(urls, 1, 1, "Component Status: GREEN").contains("BLUE,http://www.yahoo.com"));
